187c2e0e891b9c43066c25882cb204108624fca8
Manish Tirkey <asdmamanish@gmail.com>
2024-07-06 11:58:15 +0530

    ui update on chat

--- PyChat/urls.py
+++ PyChat/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from registration import views as rv
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("chat.urls")),
     path('signup/', rv.SignUp, name="register"),
     path("", include("django.contrib.auth.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
